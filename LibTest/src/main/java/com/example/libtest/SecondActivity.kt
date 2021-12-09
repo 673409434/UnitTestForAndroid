df620737750b2e76038dd39de60451dc36886d6a
hehao3 <hehao3@jd.com>
2021-12-09 11:03:20 +0800

    配置优化

--- LibTest/src/main/java/com/example/libtest/SecondActivity.kt
+++ LibTest/src/main/java/com/example/libtest/SecondActivity.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package com.example.libtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,7 +6,7 @@ import android.os.Bundle
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         getTestStr()
     }
 
